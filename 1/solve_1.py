5a07ce0940dd48e554dd661c1812c1c0013e00f4
Justin <18606523+jlewis200@users.noreply.github.com>
2025-05-31 12:51:02 -0400

    Add day 1 part 1.

---
+++ 1/solve_1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from itertools import product
+
+
+def solve(offsets):
+    """ """
+    return sum(offsets)
+
+
+def parse(data):
+    """ """
+    return [int(line.strip()) for line in data]
+
+
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f_in:
+        return f_in.readlines()
+
+
+def main(filename, expected=None):
+    result = solve(parse(read_file(filename)))
+    print(result)
+    if expected is not None:
+        assert result == expected
+
+
+if __name__ == "__main__":
+    main("input.txt")
