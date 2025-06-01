25d1de1808b937f1e2c22d4dca20a527f9eac31e
Justin <18606523+jlewis200@users.noreply.github.com>
2025-06-01 14:01:06 -0400

    Day 12 part 2.

---
+++ 12/solve_2.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import re
+import numpy as np
+
+
+def solve(initial_state, rules):
+    """
+    A pattern stabilizes after ~100 generations.  From this point the plants
+    shift to the right by 1 every iteration.
+    """
+    generation = initial_state
+    print_generation(generation)
+
+    for _ in range(100):
+        next_generation = set()
+
+        for idx in range(-200, 200):
+            for rule in rules:
+                if should_apply(generation, rule, idx):
+                    next_generation.add(idx)
+                    break
+
+        generation = next_generation
+        print_generation(generation)
+
+    return sum({element + (50000000000 - 100) for element in generation})
+
+
+def should_apply(generation, rule, idx):
+    apply = True
+
+    for offset, value in rule.items():
+
+        if value:
+            apply &= (idx + offset) in generation
+
+        else:
+            apply &= (idx + offset) not in generation
+
+    return apply
+
+
+def print_generation(generation):
+    string = ""
+
+    for idx in range(-200, 1 + max(generation)):
+        if idx in generation:
+            string += "#"
+        else:
+            string += "."
+
+    print(string)
+
+
+def parse(data):
+    """ """
+    line = data.pop(0).strip()
+    line = line.removeprefix("initial state: ")
+    initial_state = set()
+
+    for idx, pot in enumerate(line):
+        if pot == "#":
+            initial_state.add(idx)
+
+    data.pop(0)
+    rules = []
+
+    for line in data:
+        pattern = r"(?P<state>.+) => (?P<outcome>.+)"
+        match = re.fullmatch(pattern, line.strip())
+
+        if match.group("outcome") == "#":
+            rule = dict(
+                zip([-2, -1, 0, 1, 2], np.array(list(match.group("state"))) == "#")
+            )
+            rules.append(rule)
+
+    return initial_state, rules
+
+
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f_in:
+        return f_in.readlines()
+
+
+def main(filename, expected=None):
+    result = solve(*parse(read_file(filename)))
+    print(result)
+    if expected is not None:
+        assert result == expected
+
+
+if __name__ == "__main__":
+    main("input.txt")
