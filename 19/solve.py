3189bcff9599d6fe0a4792bda74295806a52949e
Justin <18606523+jlewis200@users.noreply.github.com>
2025-11-30 14:06:03 -0500

    Add day 19 part 1.

---
+++ 19/solve.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+import re
+from dataclasses import dataclass
+from collections import defaultdict
+import pandas as pd
+from itertools import pairwise
+import numpy as np
+import json
+
+
+@dataclass
+class Instruction:
+    opcode: str
+    input_0: int
+    input_1: int
+    output: int
+
+
+@dataclass
+class Sample:
+    before: list[int]
+    after: list[int]
+    instruction: Instruction
+
+
+class Interpreter:
+
+    def __init__(self, ip_register, program, registers=None):
+        self.registers = [0, 0, 0, 0, 0, 0] if registers is None else registers.copy()
+        self.ip_register = ip_register
+        self.program = program
+        self.opcode_map = {
+            "addr": self.addr,
+            "addi": self.addi,
+            "mulr": self.mulr,
+            "muli": self.muli,
+            "banr": self.banr,
+            "bani": self.bani,
+            "borr": self.borr,
+            "bori": self.bori,
+            "setr": self.setr,
+            "seti": self.seti,
+            "gtir": self.gtir,
+            "gtri": self.gtri,
+            "gtrr": self.gtrr,
+            "eqir": self.eqir,
+            "eqri": self.eqri,
+            "eqrr": self.eqrr,
+        }
+        self.remapped_opcodes = {}
+
+    def run(self):
+        while self.registers[self.ip_register] < len(self.program):
+            self.perform_instruction(self.program[self.registers[self.ip_register]])
+            self.registers[self.ip_register] += 1
+
+    def perform_instruction(self, instruction):
+        self.opcode_map[instruction.opcode](instruction)
+        # print(instruction)
+
+    def addr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] + self.registers[instruction.input_1]
+        )
+
+    def addi(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] + instruction.input_1
+        )
+
+    def mulr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] * self.registers[instruction.input_1]
+        )
+
+    def muli(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] * instruction.input_1
+        )
+
+    def banr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] & self.registers[instruction.input_1]
+        )
+
+    def bani(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] & instruction.input_1
+        )
+
+    def borr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] | self.registers[instruction.input_1]
+        )
+
+    def bori(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] | instruction.input_1
+        )
+
+    def setr(self, instruction):
+        self.registers[instruction.output] = self.registers[instruction.input_0]
+
+    def seti(self, instruction):
+        self.registers[instruction.output] = instruction.input_0
+
+    def gtir(self, instruction):
+        self.registers[instruction.output] = (
+            instruction.input_0 > self.registers[instruction.input_1]
+        )
+
+    def gtri(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] > instruction.input_1
+        )
+
+    def gtrr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] > self.registers[instruction.input_1]
+        )
+
+    def eqir(self, instruction):
+        self.registers[instruction.output] = (
+            instruction.input_0 == self.registers[instruction.input_1]
+        )
+
+    def eqri(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] == instruction.input_1
+        )
+
+    def eqrr(self, instruction):
+        self.registers[instruction.output] = (
+            self.registers[instruction.input_0] == self.registers[instruction.input_1]
+        )
+
+
+def solve(ip_register, program):
+    """ """
+    interpreter = Interpreter(ip_register, program, registers=[0, 0, 0, 0, 0, 0])
+    interpreter.run()
+    return interpreter.registers[0]
+
+
+def parse(lines):
+    """ """
+    line = lines.pop(0)
+    ip_register = int(re.match(r".*(?P<ip_register>\d+).*", line).group("ip_register"))
+
+    program = []
+
+    for line in lines:
+        opcode, *args = line.strip().split()
+        input_0, input_1, output = list(map(int, args))
+        program.append(Instruction(opcode, input_0, input_1, output))
+
+    return ip_register, program
+
+
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f_in:
+        return f_in.readlines()
+
+
+def main(filename, expected=None):
+    result = solve(*parse(read_file(filename)))
+    print(result)
+    if expected is not None:
+        assert result == expected
+
+
+if __name__ == "__main__":
+    main("test_0.txt", 7)
+    main("input.txt")
