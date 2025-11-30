41072c47fb9ad47c32a4ef6f31a9730bd74899ed
Justin <18606523+jlewis200@users.noreply.github.com>
2025-11-30 14:11:20 -0500

    Add day 19 part 2.

---
+++ 19/solve_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+The program finds the sum of all factors of the provided constant.  The main
+work for part 2 is reversing the program to determine the algorithm so it can
+be rewritten in a more efficient manner.  See 'program_re.odg' for control
+flow graph and details.
+"""
+
+constant = 1028
+constant = 10551428
+total = 0
+
+for idx in range(1, 1 + constant):
+    if constant % idx == 0:
+        total += idx
+
+print(total)
